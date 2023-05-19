ca607fefef7d70437aedd459c07b87b1b845bf1d
m1212e <->
2023-05-19 14:32:08 +0200

    wip sessions

--- chase/backend/src/main.ts
+++ chase/backend/src/main.ts
@@ -2,39 +2,97 @@ import Fastify, { FastifyInstance } from "fastify";
 import swagger from "@fastify/swagger";
 import swaggerUi from "@fastify/swagger-ui";
 import fastifyNow from "fastify-now";
+import fastifySession from "@fastify/session";
+import fastifyCookie from "@fastify/cookie";
 import { join } from "path";
-import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import dotenv from "dotenv";
 
-//TODO get from env vars
-const PORT = 3000;
+// ╔═══════════════╗
+// ║ Configuration ║
+// ╚═══════════════╝
+
+const LOAD_ENV_VARS_FROM_FILE =
+  process.argv.find((a) => a === "--load-env-vars-from-file") !== undefined;
+
+if (LOAD_ENV_VARS_FROM_FILE) {
+  dotenv.config();
+}
+
+let PORT = 0;
+if (process.env.PORT === undefined) {
+  throw new Error(
+    "Please make sure the PORT environment variable is set to a valid port number",
+  );
+}
+PORT = Number.parseInt(process.env.PORT);
+
+// ╔════════════════════════╗
+// ║ Creating server object ║
+// ╚════════════════════════╝
 
 const server: FastifyInstance = Fastify({
-  logger: true,
+  logger: { level: "warn" }, //TODO make the logger non ugly: https://www.fastify.io/docs/latest/Reference/Logging/
 }).withTypeProvider<TypeBoxTypeProvider>();
 
-server.register(swagger);
-server.register(swaggerUi, {
-  routePrefix: "/documentation",
-  uiConfig: {
-    docExpansion: "full",
-    deepLinking: false,
-  },
+// ╔═══════════════════════════════════════════════════╗
+// ║ API documentation generation & serving (dev only) ║
+// ╚═══════════════════════════════════════════════════╝
+
+const SERVE_DOCUMENTATION =
+  process.argv.find((a) => a === "--serve-documentation") !== undefined;
+
+if (SERVE_DOCUMENTATION) {
+  server.register(swagger);
+  server.register(swaggerUi, {
+    routePrefix: "/documentation",
+    uiConfig: {
+      docExpansion: "full",
+      deepLinking: false,
+    },
+  });
+}
+
+// ╔═════════════════════════════════════════╗
+// ║ Cookie parsing & Session initialization ║
+// ╚═════════════════════════════════════════╝
+
+server.register(fastifyCookie);
+const sessionSecret = process.env.SESSION_SECRET;
+if (sessionSecret === undefined) {
+  throw new Error(
+    "Could not find session secret in environment variable. Make sure that the 'SESSION_SECRET' environment variable is set and a secure string with more than 21 characters.",
+  );
+}
+server.register(fastifySession, {
+  secret: sessionSecret,
 });
+
+// ╔══════════════════════════════════════╗
+// ║ File system based route registration ║
+// ╚══════════════════════════════════════╝
 
 server.register(fastifyNow, {
   routesFolder: join(__dirname, "./routes"),
   pathPrefix: "/api",
 });
 
+// ╔════════════════╗
+// ║ Run the server ║
+// ╚════════════════╝
+
 // wrap in an async function to allow top level async
 (async () => {
   try {
     await server.ready();
-    server.swagger();
+    if (SERVE_DOCUMENTATION) {
+      server.swagger();
+    }
     await server.listen({ port: PORT, host: "0.0.0.0" });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
   }
 })();
+
 server.log.info(`server listening on ${server.server.address()}`);
--- chase/backend/src/routes/login.ts
+++ chase/backend/src/routes/login.ts
@@ -1,20 +1,39 @@
 import { NowRequestHandler } from "fastify-now";
 import { Static, Type } from "@sinclair/typebox";
 
-const User = Type.Object({
+const Body = Type.Object({
   password: Type.String(),
   email: Type.String({ format: "email" }),
 });
-export type UserType = Static<typeof User>;
+export type BodyType = Static<typeof Body>;
+
+const Reply = Type.Object({
+  firstName: Type.String(),
+  lastName: Type.String(),
+});
+export type ReplyType = Static<typeof Reply>;
+
+const ErrorResponse = Type.Union([
+  Type.Literal("CouldNotFindUser"),
+  Type.Literal("InvalidPassword"),
+]);
 
 export const POST: NowRequestHandler<{
-  Body: UserType;
+  Body: BodyType;
+  Reply: ReplyType;
 }> = async (req, rep) => {
-  return "cool, it works";
+  return {
+    firstName: "FirstName",
+    lastName: "LastName",
+  };
 };
 
 POST.opts = {
   schema: {
-    body: User,
+    body: Body,
+    response: {
+      200: Reply,
+      401: ErrorResponse,
+    },
   },
 };

1e7e67b5362ac70ba80cdb52a5d16b37111782cb
m1212e <14091540+m1212e@users.noreply.github.com>
2023-05-24 21:17:32 +0200

    Add frontend

---
+++ chase/frontend/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.scss";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Chase",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ chase/frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <button type="button" className="btn btn-primary">
+        Button
+      </button>
+    </main>
+  );
+}

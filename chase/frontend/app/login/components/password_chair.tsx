2f0d5758c242c0c4e7360e6642b6b17c5c915577
Tade Strehk <tade99@gmx.de>
2023-05-27 22:52:37 +0200

    🐞 fix: Flexbox fix

--- chase/frontend/app/login/components/password_chair.tsx
+++ chase/frontend/app/login/components/password_chair.tsx
@@ -33,7 +33,7 @@ export default function usernameLogin({ changeLoginState }) {
   };
 
   return (
-    <div className="flex flex-col p-5 items-center">
+    <div className="flex flex-col p-5 items-center justify-center h-full">
       {/* TODO This form can be a component that is used in both chair and participant login password components */}
       <form onSubmit={handleLogin(onSubmit)} className="contents">
         <Password

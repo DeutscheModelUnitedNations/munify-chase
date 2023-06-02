eaaf6e98db3fb96830e3618d8f9381c6bc4817a3
Tade Strehk <tade99@gmx.de>
2023-06-02 11:07:37 +0200

    🐞 fix: width button bug

--- chase/frontend/components/navbar/button.tsx
+++ chase/frontend/components/navbar/button.tsx
@@ -18,7 +18,7 @@ export default function NavbarButton({
       setWrapperStyle(`${defaultWrapperStyle} bg-dmunlight text-white`);
     } else {
       setWrapperStyle(
-        `${defaultWrapperStyle} bg-dmun text-dmunlight hover:bg-dmunlight hover:text-white cursor-pointer`,
+        `${defaultWrapperStyle} bg-dmun text-dmunlight hover:bg-dmunlight hover:text-white transition cursor-pointer`,
       );
     }
   }, [active]);
@@ -29,7 +29,7 @@ export default function NavbarButton({
         <FontAwesomeIcon icon={icon} className=" text-xl m-3" onClick={onClick} />
       </div>
     ) : (
-      <Link href={link}>
+      <Link href={link} className="w-full">
         <div className={wrapperStyle}>
             <FontAwesomeIcon icon={icon} className=" text-xl m-3" />
         </div>

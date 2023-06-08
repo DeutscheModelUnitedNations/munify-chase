64fd0e1d6c0b791f076b256b422213e4462da6ba
Tade Strehk <tade99@gmx.de>
2023-06-09 00:46:52 +0200

    🐞 fix: DocumentId renamed (clearer variable name)

--- chase/frontend/components/dashboard/documents/draft.tsx
+++ chase/frontend/components/dashboard/documents/draft.tsx
@@ -5,15 +5,15 @@ import { faFileLines } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 
 export default function Draft({
-  number,
+  documentId,
   introducedBy,
   sponsors,
-}: { number: string; introducedBy: string; sponsors: string[] }) {
+}: { documentId: string; introducedBy: string; sponsors: string[] }) {
   return (
     <DocumentTemplate>
       <FontAwesomeIcon icon={faFileLines} className="text-gray-400 text-2xl" />
       <div className="flex-1 flex-col justify-start items-center">
-        <div className="text-sm font-semibold text-gray-600">{number}</div>
+        <div className="text-sm font-semibold text-gray-600">{documentId}</div>
         <div className="text-xs text-gray-400">{`${sponsors.length} signierte Unterstützer`}</div>
       </div>
       <div className="flex-col justify-end items-center rounded-md border border-black shadow-md overflow-hidden">

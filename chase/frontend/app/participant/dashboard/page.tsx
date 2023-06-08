354ea58f0da7068d248d30de3abb1ef32d9b9bec
Tade Strehk <tade99@gmx.de>
2023-06-09 00:13:48 +0200

    🔵 other: Markdown css file transfer

--- chase/frontend/app/participant/dashboard/page.tsx
+++ chase/frontend/app/participant/dashboard/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import Navbar from "@/components/navbar/navbar";
-import "./markdown.scss";
 import { ScrollPanel } from "primereact/scrollpanel";
 
 import DashboardHeader from "@/components/dashboard/header";

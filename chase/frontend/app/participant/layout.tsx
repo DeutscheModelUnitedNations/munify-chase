bd9e5de13bcc947310b889069d53ced751d6bdaf
Tade Strehk <tade99@gmx.de>
2023-06-13 19:24:33 +0200

    🐞 fix: Nested Layout Mistake

--- chase/frontend/app/participant/layout.tsx
+++ chase/frontend/app/participant/layout.tsx
@@ -1,38 +1,18 @@
-import "@/app/globals.scss";
-import { Inter } from "next/font/google"; // TODO Remove Google Fonts and use local fonts (legal reasons)
 import Navbar from "@/components/navbar/navbar";
 
-//theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-//core
-import "primereact/resources/primereact.min.css";
-//icons
-import "primeicons/primeicons.css";
-
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Chase",
-};
-
-export default function RootLayout({
+export default function Participant_Pages_Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="de">
-      <body className={inter.className}>
-          <div className="flex h-screen w-screen bg-gray-light">
-            <div className="m-3 rounded-3xl bg-white flex shadow-md w-full overflow-hidden">
-              <div className="flex-1 flex">
-                <Navbar />
-                {children}
-              </div>
-            </div>
-          </div>
-      </body>
-    </html>
+    <div className="flex h-screen w-screen bg-gray-light">
+      <div className="m-3 rounded-3xl bg-white flex shadow-md w-full overflow-hidden">
+        <div className="flex-1 flex">
+          <Navbar />
+          {children}
+        </div>
+      </div>
+    </div>
   );
 }

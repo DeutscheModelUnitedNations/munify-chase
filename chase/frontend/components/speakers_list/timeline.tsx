7f43198defaac6452970f721a263243421524efb
Tade Strehk <tade99@gmx.de>
2023-06-14 10:46:46 +0200

    🐞 fix: Linter Complaining about usage of any and array index

--- chase/frontend/components/speakers_list/timeline.tsx
+++ chase/frontend/components/speakers_list/timeline.tsx
@@ -1,22 +1,18 @@
 import { CountryCode } from "@/custom_types";
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-export default function Timeline({
-  list,
-  content,
-}: { list: CountryCode[]; content: (item: any) => React.ReactNode }) {
+interface TimelineProps {
+  list: CountryCode[];
+  content: (item: CountryCode) => React.ReactNode;
+}
+
+export default function Timeline({ list, content }: TimelineProps) {
   return (
     <>
       <div className="flex-1 flex flex-col">
-        {list.map((item, index) => {
-          return (
-            <div key={index} className="flex flex-col items-start">
-              {content(item)}
-            </div>
-          );
-        })}
+        {list.map((item) => (
+          <div className="flex flex-col items-start">{content(item)}</div>
+        ))}
       </div>
     </>
   );

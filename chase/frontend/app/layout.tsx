97806716b6983862fc1fa57c03740e495b5df600
Tade Strehk <tade99@gmx.de>
2023-06-14 10:06:19 +0200

    🐞 temp fix: Metadata, i18n & use Client Directive

--- chase/frontend/app/layout.tsx
+++ chase/frontend/app/layout.tsx
@@ -1,4 +1,4 @@
-
+"use client";
 import "./globals.scss";
 import { Inter } from "next/font/google"; // TODO Remove Google Fonts and use local fonts (legal reasons)
 
@@ -15,19 +15,28 @@ import TypesafeI18n from "@/src/i18n/i18n-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Chase",
-};
+// export const metadata = {
+//   title: "Chase",
+// }; // TODO find a way to add Metadata to the client side generated pages. static metadata does not work with nextjs https://nextjs.org/docs/getting-started/react-essentials#the-use-client-directive
 
-"use client";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // https://github.com/ivanhofer/typesafe-i18n/tree/main/packages/detectors)
-  const locale = detectLocale(baseLocale, locales, navigatorDetector);
-  loadLocale(locale);
+  // TODO inspect the way locale is detected and loaded.
+  // The Problem was that the locale was not loaded on the client side. There was a build error because the navigator was not defined on the server side.
+  // // https://github.com/ivanhofer/typesafe-i18n/tree/main/packages/detectors)
+  // const locale = detectLocale(baseLocale, locales, navigatorDetector);
+  // loadLocale(locale);
+
+  // TadeSF 2023-06-14 suggested solution:
+  let locale = baseLocale; // default to baseLocale
+  if (typeof window !== "undefined") {
+    // Check if window is defined to ensure running on client side
+    locale = detectLocale(baseLocale, locales, navigatorDetector);
+    loadLocale(locale);
+  }
 
   return (
     <TypesafeI18n locale={locale}>

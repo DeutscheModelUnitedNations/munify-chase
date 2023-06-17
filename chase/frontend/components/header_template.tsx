9bacee5317ee5a87233f78649a28f0a5c0486054
Tade Strehk <tade99@gmx.de>
2023-06-17 11:09:44 +0200

    📄 docs: Component Description

--- chase/frontend/components/header_template.tsx
+++ chase/frontend/components/header_template.tsx
@@ -3,6 +3,9 @@ import React from "react";
 export default function HeaderTemplate({
   children,
 }: { children: React.ReactNode }) {
+  // This is a style component. It is used to create a header div for pages Like the dashboard.
+
+
   return (
     <>
       <div className=" h-32 bg-gray-300 flex justify-between items-center p-4">

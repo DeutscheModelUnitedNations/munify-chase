7c4d3f1c9f18c9076cd77e18d10f6296097e94cb
Tade Strehk <tade99@gmx.de>
2023-06-17 11:13:06 +0200

    ✨feature: Linter

--- chase/frontend/components/header_template.tsx
+++ chase/frontend/components/header_template.tsx
@@ -5,7 +5,6 @@ export default function HeaderTemplate({
 }: { children: React.ReactNode }) {
   // This is a style component. It is used to create a header div for pages Like the dashboard.
 
-
   return (
     <>
       <div className=" h-32 bg-gray-300 flex justify-between items-center p-4">

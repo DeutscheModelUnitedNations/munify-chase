906c6e1de95f0ee98b956d4cbb85967748c07efc
m1212e <14091540+m1212e@users.noreply.github.com>
2023-10-03 00:38:33 +0200

    🐞 fix (CHASE Frontend): match versions to prevent type issues in prod build branch: elysia-2

--- chase/frontend/components/speakers_list/speech_buttons.tsx
+++ chase/frontend/components/speakers_list/speech_buttons.tsx
@@ -180,7 +180,7 @@ export function ChairSpeechButtons({
         >
           <AddSpeakerOverlay
             listOfAllCountries={listOfAllCountries}
-            listClosed={listClosed}
+            _listClosed={listClosed}
             closeOverlay={() => setAddSpeakersOverlayVisible(false)}
             typeOfList={typeOfList}
           />

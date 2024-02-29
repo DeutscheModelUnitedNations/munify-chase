88c06ff9d8e092a715b901b3c07ec89f818e6c59
m1212e <14091540+m1212e@users.noreply.github.com>
2024-02-29 12:41:16 +0100

    wip: auto convert dates to string

--- chase/backend/src/util/nullToUndefined.ts
+++ chase/backend/src/util/nullToUndefined.ts
@@ -1,17 +1,26 @@
-type UndefinedIfNull<T> = T extends null ? undefined : T extends (infer U)[] ? UndefinedIfNull<U>[] : T extends object ? { [K in keyof T]: UndefinedIfNull<T[K]> } : T;
+type UndefinedIfNull<T> = T extends null
+  ? undefined
+  : T extends (infer U)[]
+    ? UndefinedIfNull<U>[]
+    : T extends object
+      ? { [K in keyof T]: UndefinedIfNull<T[K]> }
+      : T;
 
-export function nullToUndefined<T>(obj: T): UndefinedIfNull<T> {
+export function recursiveNullFieldsToUndefined<T>(obj: T): UndefinedIfNull<T> {
   if (obj === null) {
+    // biome-ignore lint/suspicious/noExplicitAny:
     return undefined as any;
   }
 
   if (typeof obj === "object") {
+    // biome-ignore lint/suspicious/noExplicitAny:
     const newObj: any = Array.isArray(obj) ? [] : {};
     for (const key in obj) {
-      newObj[key] = nullToUndefined(obj[key]);
+      newObj[key] = recursiveNullFieldsToUndefined(obj[key]);
     }
     return newObj;
   }
 
+  // biome-ignore lint/suspicious/noExplicitAny:
   return obj as any;
 }

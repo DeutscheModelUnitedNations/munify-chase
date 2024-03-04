63357b94df500ed007e8114aa7eda6aec304282d
m1212e <14091540+m1212e@users.noreply.github.com>
2024-03-04 05:04:09 +0100

    🏗️ build (MUNify): add migration container branch: deployment

--- chase/frontend/services/backend.ts
+++ chase/frontend/services/backend.ts
@@ -1,11 +1,19 @@
 import { edenTreaty } from "@elysiajs/eden";
 import { App } from "../../backend/src/main";
+import { unstable_noStore as noStore } from "next/cache";
 
-export const backend = edenTreaty<App>(
-  process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:3001",
-  {
-    $fetch: {
-      credentials: "include",
-    },
+function getBackendUrl() {
+  noStore();
+  if (!process.env.NEXT_PUBLIC_BACKEND_URL) {
+    console.warn("Could not read production backend url");
+  }
+  //TODO how the hell do we do this in next without hardcoding?!
+  // return process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:3001";
+  return "https://chase-backend.dmun.de";
+}
+
+export const backend = edenTreaty<App>(getBackendUrl(), {
+  $fetch: {
+    credentials: "include",
   },
-);
+});

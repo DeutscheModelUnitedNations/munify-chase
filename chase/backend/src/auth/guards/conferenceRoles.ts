b60d42e0b1a8db10630ddc2220c267fb44e357eb
m1212e <14091540+m1212e@users.noreply.github.com>
2024-03-08 02:25:22 +0100

    🐞 fix (CHASE Backend): hotfix disable all guards branch: deployment

--- chase/backend/src/auth/guards/conferenceRoles.ts
+++ chase/backend/src/auth/guards/conferenceRoles.ts
@@ -1,14 +1,15 @@
-import Elysia, { t } from "elysia";
+// import Elysia, { t } from "elysia";
+import Elysia from "elysia";
 import { session } from "../session";
-import { TypeCompiler } from "@sinclair/typebox/compiler";
-import { db } from "../../../prisma/db";
+// import { TypeCompiler } from "@sinclair/typebox/compiler";
+// import { db } from "../../../prisma/db";
 import { ConferenceRole } from "../../../prisma/generated/client";
 
-const parametersSchema = TypeCompiler.Compile(
-  t.Object({
-    conferenceId: t.String(),
-  }),
-);
+// const parametersSchema = TypeCompiler.Compile(
+//   t.Object({
+//     conferenceId: t.String(),
+//   }),
+// );
 
 export const conferenceRoleGuard = new Elysia({
   name: "conferenceRoleGuard",
@@ -20,31 +21,32 @@ export const conferenceRoleGuard = new Elysia({
        * Checks if a user has the role in the conference. You can set the role to "any" to check if the user has any role in the conference.
        * You can also set the role to an array of roles to check if the user has any of the roles in the conference.
        */
-      hasConferenceRole(roles: ConferenceRole[] | "any") {
-        onBeforeHandle(async ({ session, set, params }) => {
-          if (session.loggedIn !== true || !session.userData) {
-            // biome-ignore lint/suspicious/noAssignInExpressions: This is a valid use case
-            return (set.status = "Unauthorized");
-          }
+      hasConferenceRole(_roles: ConferenceRole[] | "any") {
+        onBeforeHandle(async () => {
+        // onBeforeHandle(async ({ session, set, params }) => {
+          // if (session.loggedIn !== true || !session.userData) {
+          //   // biome-ignore lint/suspicious/noAssignInExpressions: This is a valid use case
+          //   return (set.status = "Unauthorized");
+          // }
 
-          if (!parametersSchema.Check(params)) {
-            set.status = "Bad Request";
-            return [...parametersSchema.Errors(params)];
-          }
-          const { conferenceId } = parametersSchema.Decode(params);
+          // if (!parametersSchema.Check(params)) {
+          //   set.status = "Bad Request";
+          //   return [...parametersSchema.Errors(params)];
+          // }
+          // const { conferenceId } = parametersSchema.Decode(params);
 
-          const res = await db.conferenceMember.findFirst({
-            where: {
-              userId: session.userData.id,
-              conferenceId,
-              role: roles === "any" ? undefined : { in: roles },
-            },
-          });
+          // const res = await db.conferenceMember.findFirst({
+          //   where: {
+          //     userId: session.userData.id,
+          //     conferenceId,
+          //     role: roles === "any" ? undefined : { in: roles },
+          //   },
+          // });
 
-          if (!res) {
-            // biome-ignore lint/suspicious/noAssignInExpressions: This is a valid use case
-            return (set.status = "Unauthorized");
-          }
+          // if (!res) {
+          //   // biome-ignore lint/suspicious/noAssignInExpressions: This is a valid use case
+          //   return (set.status = "Unauthorized");
+          // }
         });
       },
     };

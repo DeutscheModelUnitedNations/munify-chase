8d53a8122b23031681984fe4b9627b1f119a6c7b
Tade Strehk <42637916+TadeSF@users.noreply.github.com>
2024-03-18 15:28:39 +0000

    🧼 format & lint (CHASE Backend): branch: 128-password-regex-strong-but-provides-very-little-flexibility

--- chase/backend/src/routes/auth/passwords.ts
+++ chase/backend/src/routes/auth/passwords.ts
@@ -18,7 +18,6 @@ import { loggedInGuard } from "../../auth/guards/loggedIn";
 const passwordRegex =
   /^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{8,}$/;
 
-
 export const passwords = new Elysia()
   .use(loggedInGuard)
   .post(

4b51c63ecce3258957ead1fe7ce452c16c4f498a
Tade Strehk <42637916+TadeSF@users.noreply.github.com>
2024-05-05 22:32:36 +0200

    🐞 fix (App Backend): util function location error branch: main

--- chase/backend/prisma/custom_seeds/seed_munbw2024.ts
+++ chase/backend/prisma/custom_seeds/seed_munbw2024.ts
@@ -1,6 +1,6 @@
 // import { faker } from "@faker-js/faker";
 import { $Enums, PrismaClient } from "../generated/client";
-import { alpha2ToAlpha3 } from "../../util/countryCodeUtils";
+import { alpha2ToAlpha3 } from "../countryCodeUtils";
 import delegationData from "./munbw2024_delegations.json";
 const prisma = new PrismaClient();
 

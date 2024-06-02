e65ca44bcbd9c4daab69a7c8ef9598c43f3fb329
m1212e <14091540+m1212e@users.noreply.github.com>
2024-06-02 17:08:35 +0200

    Untracked files that are now ignored by .gitignore

--- prisma/generated/graphql/User/queries/index.ts
+++
@@ -1,7 +0,0 @@
-export { findFirstUserQuery, findFirstUserQueryObject } from "./findFirst.base";
-export { findManyUserQuery, findManyUserQueryObject } from "./findMany.base";
-export { countUserQuery, countUserQueryObject } from "./count.base";
-export {
-  findUniqueUserQuery,
-  findUniqueUserQueryObject,
-} from "./findUnique.base";

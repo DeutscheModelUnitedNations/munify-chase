9d083738f2fbb6a5a33f8f26a478d9f06905881a
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2024-08-20 17:20:13 +0000

    style: changed the default font size in the whiteboard in dashboard and presentation mode

--- chase/frontend/components/whiteboard/whiteboard.tsx
+++ chase/frontend/components/whiteboard/whiteboard.tsx
@@ -1,16 +1,7 @@
 import { Editor } from "primereact/editor";
 import { Skeleton } from "primereact/skeleton";
-import React from "react";
-
-/**
- * This Component is a wrapper for the PrimeReact Editor Component.
- * It allows easy usage of the Editor Component as a Whiteboard.
- * @param value The value (content / text) of the Whiteboard in html format
- * @param readOnly Whether the Whiteboard is read only or not. When readOnly is true, the Header of the Editor is hidden.
- * @param setContentFunction The function that is called when the content of the Whiteboard changes
- * @param rest The rest of the props that are passed to the PrimeReact Editor Component
- * @returns A Whiteboard Component
- */
+import React, { useEffect } from "react";
+import "./whiteboard.css";
 
 export default function ChairWhiteboard({
   value,

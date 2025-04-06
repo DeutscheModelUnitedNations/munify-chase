34fe5e499046f761ad96b54aa6d58d503716438a
m1212e <14091540+m1212e@users.noreply.github.com>
2025-04-06 22:25:19 +0200

    ↩️ revert: messages from demo

--- src/api/handlers/user.ts
+++ src/api/handlers/user.ts
@@ -1,8 +1,5 @@
 import { abilityBuilder, object, pubsub, query, schemaBuilder } from '$api/rumble';
 
-// TODO
-abilityBuilder.user.allow('read');
-
 const UserRef = object({
 	name: 'User',
 	tableName: 'user'

e59b878a6bab32bc430b1230f2e9b9580c7fc97d
m1212e <14091540+m1212e@users.noreply.github.com>
2025-04-15 18:49:23 +0200

    🚧 wip: presence setter

--- src/api/handlers/committeeMember.ts
+++ src/api/handlers/committeeMember.ts
@@ -1,3 +1,44 @@
+import { db, schema } from '$api/db/db';
+import { schemaBuilder } from '$api/rumble';
+import { and, eq } from 'drizzle-orm';
 import { basics } from './basics';
+import { assertFindFirstExists } from '@m1212e/rumble';
 
 const { arg, ref, pubsub } = basics('committeeMember');
+
+schemaBuilder.mutationFields((t) => {
+	return {
+		setPresenceForCommitteeMember: t.drizzleField({
+			type: ref,
+			args: {
+				id: t.arg.id({ required: true }),
+				present: t.arg.boolean({ required: true })
+			},
+			resolve: async (query, root, args, ctx, info) => {
+				await db
+					.update(schema.committeeMember)
+					.set({
+						present: args.present
+					})
+					.where(
+						and(
+							eq(schema.committeeMember.id, args.id),
+							ctx.abilities.committeeMember.filter('update').single.where
+						)
+					);
+
+				pubsub.updated(args.id);
+
+				return db.query.committeeMember
+					.findFirst(
+						query(
+							ctx.abilities.committeeMember.filter('read', {
+								inject: { where: { id: eq(schema.committeeMember.id, args.id) } }
+							}).single
+						)
+					)
+					.then(assertFindFirstExists);
+			}
+		})
+	};
+});
--- src/api/handlers/user.ts
+++ src/api/handlers/user.ts
@@ -3,25 +3,3 @@ import { basics } from './basics';
 
 const { arg, ref, pubsub } = basics('user');
 
-schemaBuilder.mutationFields((t) => {
-	return {
-		dummy: t.drizzleField({
-			type: ref,
-			resolve: async (query, root, args, ctx, info) => {
-				// pubsub.updated("1")
-				// TODO
-				return {
-					id: '1',
-					name: 'John Doe',
-					createdAt: new Date(),
-					email: 'FZVr1@example.com',
-					familyName: 'Doe',
-					givenName: 'John',
-					locale: 'en',
-					preferredUsername: 'johndoe',
-					updatedAt: new Date()
-				};
-			}
-		})
-	};
-});

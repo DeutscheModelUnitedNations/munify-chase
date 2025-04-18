0622a6bd1c8a141abf61666eb5b12059e1e118eb
m1212e <14091540+m1212e@users.noreply.github.com>
2025-04-18 14:35:18 +0200

    🧼 format & lint

--- src/api/rumble.ts
+++ src/api/rumble.ts
@@ -2,7 +2,8 @@ import { rumble } from '@m1212e/rumble';
 import { db } from './db/db';
 import { context } from './context';
 
-export const { abilityBuilder, schemaBuilder, arg, object, query, pubsub, createYoga, enum_ } = rumble({
-	db,
-	context
-});
+export const { abilityBuilder, schemaBuilder, arg, object, query, pubsub, createYoga, enum_ } =
+	rumble({
+		db,
+		context
+	});

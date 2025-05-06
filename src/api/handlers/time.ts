b15016395edaa2c05264540dbfb0efefef3297dd
m1212e <14091540+m1212e@users.noreply.github.com>
2025-05-07 00:55:40 +0200

    ✨ feat: time concept

--- src/api/handlers/time.ts
+++ src/api/handlers/time.ts
@@ -4,6 +4,7 @@ schemaBuilder.queryFields((t) => {
 	return {
 		serverTime: t.field({
 			type: 'DateTime',
+			nullable: false,
 			resolve: (root, args, ctx) => {
 				ctx.mustBeLoggedIn();
 				return new Date();

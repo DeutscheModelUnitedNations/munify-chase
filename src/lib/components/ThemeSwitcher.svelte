53dd8e14b5eea6c531fa94c015a622cce913ec67
Tade Strehk <git@strehk.eu>
2025-05-07 23:19:40 +0200

    feat: add new localization strings and enhance LanguageSwitcher and ThemeSwitcher components

--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -1,15 +1,40 @@
 <script lang="ts">
 	import { getTheme, toggleTheme } from '$lib/utils/theme.svelte';
+	import { fly } from 'svelte/transition';
 </script>
 
-<button class="btn flex-1" onclick={() => toggleTheme()}>
+<button class="btn btn-lg relative flex-1" onclick={() => toggleTheme()}>
 	{#if getTheme() === 'dark'}
-		<i class="fa-duotone fa-moon w-6 text-center"></i>
+		<div
+			class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center"
+			in:fly={{ x: -20 }}
+			out:fly={{ x: 20 }}
+		>
+			<i class="fa-duotone fa-moon w-6 text-center"></i>
+		</div>
 	{:else if getTheme() === 'light'}
-		<i class="fa-duotone fa-sun w-6 text-center"></i>
+		<div
+			class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center"
+			in:fly={{ x: -20 }}
+			out:fly={{ x: 20 }}
+		>
+			<i class="fa-duotone fa-sun w-6 text-center"></i>
+		</div>
 	{:else if getTheme() === 'system'}
-		<i class="fa-duotone fa-desktop w-6 text-center"></i>
+		<div
+			class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center"
+			in:fly={{ x: -20 }}
+			out:fly={{ x: 20 }}
+		>
+			<i class="fa-duotone fa-desktop w-6 text-center"></i>
+		</div>
 	{:else}
-		<i class="fa-duotone fa-question w-6 text-center"></i>
+		<div
+			class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center"
+			in:fly={{ x: -20 }}
+			out:fly={{ x: 20 }}
+		>
+			<i class="fa-duotone fa-question w-6 text-center"></i>
+		</div>
 	{/if}
 </button>

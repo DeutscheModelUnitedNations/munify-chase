d0f1ab37344153c3f4b5c7d693677019224b15db
Tade Strehk <git@strehk.eu>
2025-05-15 22:35:16 +0200

    feat: Enhance AddSpeakers component with improved filtering and hotkey support; update speaker addition logic and UI elements

--- src/lib/components/speakersList/chairControls/AddSpeakers.svelte
+++ src/lib/components/speakersList/chairControls/AddSpeakers.svelte
@@ -1,33 +1,119 @@
 <script lang="ts">
-	import type { CommitteeTeamQuery$result } from '$houdini';
+	import { representation } from '$api/db/schema';
+	import { browser } from '$app/environment';
+	import { graphql, type CommitteeTeamQuery$result } from '$houdini';
 	import Combobox from '$lib/components/Combobox.svelte';
 	import Flag from '$lib/components/Flag.svelte';
+	import { m } from '$lib/paraglide/messages';
 	import { getTranslatedCountryNameFromAlpha3Code } from '$lib/utils/nationTranslationHelper.svelte';
+	import { promiseToastStrings } from '$lib/utils/toast';
+	import Fuse, { type IFuseOptions } from 'fuse.js';
+	import hotkeys from 'hotkeys-js';
+	import toast from 'svelte-french-toast';
 
 	interface Props {
-		speakersList?: NonNullable<
-			CommitteeTeamQuery$result['findFirstCommittee']['activeAgendaItem']
-		>['speakersList'][number];
+		speakersList?:
+			| NonNullable<
+					CommitteeTeamQuery$result['findFirstCommittee']['activeAgendaItem']
+			  >['speakersList'][number]
+			| null;
 		members: CommitteeTeamQuery$result['findFirstCommittee']['members'];
 	}
+
+	type Member = NonNullable<typeof members>[number];
 
 	let { speakersList, members }: Props = $props();
 
 	let value = $state('');
+	let focused = $state(false);
 
-	const filter = (x: string, value: string) => x.toLowerCase().includes(value.toLowerCase());
-	const getName = (representation: NonNullable<typeof members>[number]['representation']) =>
-		representation?.name
-			? representation.name
-			: getTranslatedCountryNameFromAlpha3Code(representation?.alpha3Code);
+	const getName = (member: Member) =>
+		member.representation?.name
+			? member.representation.name
+			: getTranslatedCountryNameFromAlpha3Code(member.representation?.alpha3Code);
+
+	const fuseOptions: IFuseOptions<any> = {
+		keys: ['label'],
+		// threshold: 0.3, // Adjust the threshold for fuzzy matching
+		ignoreFieldNorm: true,
+		ignoreDiacritics: true,
+		shouldSort: true
+	};
+
+	let fuse = $state(new Fuse(members ?? [], fuseOptions));
+
+	const filter = (members: Member[], value: string) => {
+		if (value.length !== 0) {
+			fuse.setCollection(members.map((x) => ({ ...x, label: getName(x) })) ?? []);
+			const search = fuse.search(value);
+			return search.map((result) => result.item);
+		} else {
+			return members.sort((a, b) => getName(a).localeCompare(getName(b)));
+		}
+	};
+
+	const AddSpeakerToListMutation = graphql(`
+		mutation AddSpeakerToList($committeeMemberId: ID!, $speakersListId: ID!) {
+			addSpeakerOnList(committeeMemberId: $committeeMemberId, speakersListId: $speakersListId) {
+				id
+				speakersList {
+					id
+				}
+			}
+		}
+	`);
+
+	const addSpeakerToList = async () => {
+		if (!speakersList?.id) return;
+		if (!value) return;
+		const committeeMember = members.find((x) => getName(x) === value);
+
+		if (!committeeMember) {
+			return;
+		}
+
+		await toast.promise(
+			AddSpeakerToListMutation.mutate({
+				committeeMemberId: committeeMember.id,
+				speakersListId: speakersList.id
+			}),
+			promiseToastStrings(getName(committeeMember), 'add')
+		);
+
+		value = '';
+	};
+
+	$effect(() => {
+		if (!focused) {
+			hotkeys('alt+a, alt+shift+a', (event, handler) => {
+				event.preventDefault();
+				console.log('hotkey', handler.key);
+				switch (handler.key) {
+					case 'alt+a':
+						if (speakersList?.type === 'SPEAKERS_LIST') {
+							focused = true;
+						}
+						break;
+					case 'alt+shift+a':
+						if (speakersList?.type === 'COMMENT_LIST') {
+							focused = true;
+						}
+						break;
+				}
+			});
+		}
+	});
 </script>
 
 <Combobox
-	{value}
+	bind:value
+	bind:focused
 	options={members ?? []}
-	filter={({ representation }, value) => filter(getName(representation), value)}
+	filter={(member, value) => filter(member, value)}
 	placeholder="Search for a country"
-	getStringValue={({ representation }) => getName(representation)}
+	getStringValue={(member) => getName(member)}
+	kbd={speakersList?.type === 'COMMENT_LIST' ? '⌥ ⇧ A' : '⌥ A'}
+	submit={() => addSpeakerToList()}
 >
 	{#snippet ListItem(option)}
 		<Flag
@@ -37,7 +123,17 @@
 			icon={option.representation?.faIcon}
 		/>
 		<span class="ml-2">
-			{getName(option.representation)}
+			{getName(option)}
 		</span>
+	{/snippet}
+
+	{#snippet AdditionalButtons()}
+		<button
+			class="btn btn-lg btn-square join-item"
+			aria-label="add-speaker"
+			onclick={addSpeakerToList}
+		>
+			<i class="fas fa-plus"></i>
+		</button>
 	{/snippet}
 </Combobox>

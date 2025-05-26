54606c03bb876fda6ae8ff3e7130a74c17c0e83c
maximilianilzhoefer <55856340+maximilianilzhoefer@users.noreply.github.com>
2025-05-26 23:05:58 +0200

    🐞 fix: Fix simple majority when only one country is present (#230)
    Co-authored-by: Tade Strehk <github@strehk.eu>

--- src/api/handlers/committee.ts
+++ src/api/handlers/committee.ts
@@ -70,7 +70,13 @@ const ref = object({
 					return parent.customSimpleMajority;
 				}
 				const total = await getTotalPresentCount(parent as any);
-				return Math.ceil(total / 2) + 1;
+				let majority: number;
+				if ((total / 2) % 1 == 0) {
+					majority = total / 2 + 1;
+				} else {
+					majority = Math.ceil(total / 2);
+				}
+				return majority > total ? total : majority;
 			}
 		}),
 		twoThirdsMajority: t.field({

b63146c962b03ef338c7c02b3475dcf46d927f4a
m1212e <14091540+m1212e@users.noreply.github.com>
2026-03-10 13:46:25 +0100

    format: run formatter

--- src/routes/app/[conferenceId]/[committeeId]/(chairs)/resolutions/+page.svelte
+++ src/routes/app/[conferenceId]/[committeeId]/(chairs)/resolutions/+page.svelte
@@ -426,8 +426,12 @@
 														count: String(paper.sponsors.length)
 													})}
 												</span>
-												<span>{m.submittedBy()}: {paper.creator.represenation?.name ||
-													getTranslatedCountryNameFromAlpha3Code(paper.creator.represenation?.alpha3Code)}</span>
+												<span
+													>{m.submittedBy()}: {paper.creator.represenation?.name ||
+														getTranslatedCountryNameFromAlpha3Code(
+															paper.creator.represenation?.alpha3Code
+														)}</span
+												>
 											</div>
 										</a>
 										{#if paper.status === 'DRAFT_RESOLUTION' || paper.status === 'AMENDMENT_PHASE' || paper.status === 'VOTING_PHASE'}

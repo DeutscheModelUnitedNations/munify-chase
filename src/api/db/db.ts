959cdd22a777cca5c46522718d4adcbfd468465a
m1212e <14091540+m1212e@users.noreply.github.com>
2026-03-22 14:18:26 +0100

    chore: push db migration file structure to new drizzle version

--- src/api/db/db.ts
+++ src/api/db/db.ts
@@ -5,7 +5,8 @@ import { configPrivate } from '$config/private';
 
 export const db = drizzle(configPrivate.DATABASE_URL, {
 	relations: relationsInternal,
-	casing: 'snake_case'
+	casing: 'snake_case',
+	schema: schemaInternal
 });
 
 export const schema = schemaInternal;

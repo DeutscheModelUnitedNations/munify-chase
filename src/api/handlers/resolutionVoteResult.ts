c193459670dfaf51d38077a2fe0a56a7021117c1
m1212e <14091540+m1212e@users.noreply.github.com>
2026-05-19 11:49:09 +0200

    improvement: remove redundant permissions

--- src/api/handlers/resolutionVoteResult.ts
+++ src/api/handlers/resolutionVoteResult.ts
@@ -1,12 +1,11 @@
 import { abilityBuilder, object, pubsub as rumblePubsub, query } from '$api/rumble';
-import { isGlobalAdmin } from '$api/services/authHelper';
 
-abilityBuilder.resolutionVoteResult.allow('read').when((ctx) => {
-	if (isGlobalAdmin(ctx)) return 'allow';
-});
+// abilityBuilder.resolutionVoteResult.allow('read').when((ctx) => {
+// 	if (isGlobalAdmin(ctx)) return 'allow';
+// });
 
 abilityBuilder.resolutionVoteResult.allow('read').when(({ mustBeLoggedIn }) => {
-	mustBeLoggedIn();
+	const _user = mustBeLoggedIn();
 	return 'allow';
 });
 

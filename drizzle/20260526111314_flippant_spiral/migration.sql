edafd3ca1a967a0066f4cb32947c758171d28410
m1212e <14091540+m1212e@users.noreply.github.com>
2026-05-26 14:03:04 +0200

    fix migrations

--- drizzle/20260526111314_flippant_spiral/migration.sql
+++
@@ -1,6 +0,0 @@
-ALTER TYPE "vote_outcome" RENAME TO "paper_vote_outcome";--> statement-breakpoint
-ALTER TABLE "nsa_presence_event" DROP CONSTRAINT "nsa_presence_event_conference_id_conference_id_fkey";--> statement-breakpoint
-DROP INDEX "nsa_presence_event_conference_ts_idx";--> statement-breakpoint
-ALTER TABLE "nsa_presence_event" DROP COLUMN "conference_id";--> statement-breakpoint
-ALTER TABLE "conference_user" ADD CONSTRAINT "conference_user_conference_id_user_email_unique" UNIQUE("conference_id","user_email");--> statement-breakpoint
-ALTER TABLE "conference_user" ADD CONSTRAINT "conference_user_user_email_key" UNIQUE("user_email");
--- drizzle/20260526120210_cool_silver_centurion/migration.sql
+++ drizzle/20260526120210_cool_silver_centurion/migration.sql
@@ -2,6 +2,7 @@ ALTER TABLE "amendment" DROP CONSTRAINT "amendment_paper_id_resolution_paper_id_
 ALTER TABLE "amendment_sponsor" DROP CONSTRAINT "amendment_sponsor_amendment_id_amendment_id_fk";--> statement-breakpoint
 ALTER TABLE "committee" DROP CONSTRAINT "committee_active_draft_resolution_id_resolution_paper_id_fk";--> statement-breakpoint
 ALTER TABLE "committee" DROP CONSTRAINT "committee_active_amendment_id_amendment_id_fk";--> statement-breakpoint
+ALTER TABLE "nsa_presence_event" DROP CONSTRAINT "nsa_presence_event_conference_id_conference_id_fkey";--> statement-breakpoint
 ALTER TABLE "operative_clause_vote" DROP CONSTRAINT "operative_clause_vote_paper_id_resolution_paper_id_fk";--> statement-breakpoint
 ALTER TABLE "paper_content_snapshot" DROP CONSTRAINT "paper_content_snapshot_paper_id_resolution_paper_id_fk";--> statement-breakpoint
 ALTER TABLE "paper_editor" DROP CONSTRAINT "paper_editor_paper_id_resolution_paper_id_fk";--> statement-breakpoint
@@ -21,6 +22,7 @@ DROP TABLE "paper_yjs_doc";--> statement-breakpoint
 DROP TABLE "resolution_comment";--> statement-breakpoint
 DROP TABLE "resolution_paper";--> statement-breakpoint
 DROP TABLE "resolution_vote_result";--> statement-breakpoint
+DROP INDEX "nsa_presence_event_conference_ts_idx";--> statement-breakpoint
 ALTER TABLE "committee" DROP COLUMN "custom_paper_support_threshold";--> statement-breakpoint
 ALTER TABLE "committee" DROP COLUMN "last_resolution_adoption_date";--> statement-breakpoint
 ALTER TABLE "committee" DROP COLUMN "max_draft_resolutions";--> statement-breakpoint
@@ -33,9 +35,12 @@ ALTER TABLE "committee" DROP COLUMN "amendment_sponsoring_open";--> statement-br
 ALTER TABLE "committee" DROP COLUMN "active_amendment_id";--> statement-breakpoint
 ALTER TABLE "committee" DROP COLUMN "resolution_headline";--> statement-breakpoint
 ALTER TABLE "conference" DROP COLUMN "resolution_feature_enabled";--> statement-breakpoint
+ALTER TABLE "nsa_presence_event" DROP COLUMN "conference_id";--> statement-breakpoint
+ALTER TABLE "conference_user" ADD CONSTRAINT "conference_user_conference_id_user_email_unique" UNIQUE("conference_id","user_email");--> statement-breakpoint
+ALTER TABLE "conference_user" ADD CONSTRAINT "conference_user_user_email_key" UNIQUE("user_email");--> statement-breakpoint
 DROP TYPE "amendment_status";--> statement-breakpoint
 DROP TYPE "amendment_type";--> statement-breakpoint
 DROP TYPE "comment_visibility";--> statement-breakpoint
 DROP TYPE "paper_status";--> statement-breakpoint
-DROP TYPE "paper_vote_outcome";--> statement-breakpoint
-DROP TYPE "share_code_permission";
+DROP TYPE "share_code_permission";--> statement-breakpoint
+DROP TYPE "vote_outcome";
